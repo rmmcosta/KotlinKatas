dfd9c6b8efd8f074059c530d07e2fedd7bb91877
Ricardo Costa <ricardocosta101085@gmail.com>
2023-09-13 10:08:57 +0100

    feat: string range with operator extension function iterator()

---
+++ src/main/kotlin/ExtensionFunctions/StringRange.kt
@@ -0,0 +1,73 @@
+package ExtensionFunctions
+
+import kotlin.text.StringBuilder
+
+/*operator fun ClosedRange<String>.iterator() = object : Iterator<String> {
+    private val next = StringBuilder(start)
+    private val last = endInclusive
+
+    override fun hasNext(): Boolean = next.toString() <= last && next.length <= last.length
+
+    override fun next(): String {
+        //the current one must be returned, but before that we must change the
+        //string builder to accommodate the next iteration
+        val result: String = next.toString()
+        val lastChar = next.last()
+        if (lastChar < Char.MAX_VALUE) {
+            val nextChar = lastChar + 1
+            val lastIndex = next.length - 1
+            next.setCharAt(lastIndex, nextChar)
+        } else {
+            next.append(Char.MIN_VALUE)
+        }
+        return result
+    }
+}*/
+
+operator fun ClosedRange<String>.iterator() = object : Iterator<String> {
+
+    private val next: StringBuilder = StringBuilder(start)
+    private val last: String = endInclusive
+
+    override fun hasNext(): Boolean = next.toString() <= last && next.length <= last.length
+
+    override fun next(): String {
+        //the next is the one we want to return now
+        val result: String = next.toString()
+        //and we want to compute the next one to be returned
+        for ((index, c) in result.withIndex()) {
+            if (c < last[index]) {
+                next.setCharAt(index, c + 1)
+                break
+            } else {
+                if (index == result.lastIndex) {
+                    next.append(Char.MIN_VALUE)
+                }
+            }
+        }
+        return result
+    }
+}
+
+fun main() {
+    for (i in "hell".."help") {
+        println(i)
+    }
+
+    for (i in 'A'..'C') {
+        println(i)
+    }
+
+    for (i in 1..10) {
+        println(i)
+    }
+
+    for (i in "A".."Z") {
+        println(i)
+    }
+
+    for (i in "ABC".."CBC") {
+        println(i)
+    }
+
+}
