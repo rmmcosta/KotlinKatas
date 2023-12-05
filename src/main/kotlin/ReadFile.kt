d3c531a827169df9c47090475121daa884d203ef
rmmcosta <ricardocosta101085@gmail.com>
2023-12-05 08:45:24 +0000

    feat: getting the most nested file

--- src/main/kotlin/ReadFile.kt
+++ src/main/kotlin/ReadFile.kt
@@ -11,4 +11,32 @@ fun main() {
     val linesWithNumbers = lines.filter { it.contains(Regex("\\d")) }
     println("Lines with numbers: $linesWithNumbers")
     println("Lines with numbers: ${linesWithNumbers.size}")
+    val myFile = File("src${separator}main${separator}resources${separator}MyCases.txt")
+    myFile.appendText("\nThe Sign of the Four")
+    val nestedFiles: MutableMap<String, Int> = mutableMapOf()
+    val nestedDir = File("src${separator}main${separator}resources${separator}basedir")
+    nestedDir.list()?.forEach { println(it) }
+    nestedDir.walkBottomUp().forEach {
+        if (it.isFile) {
+            println("${it.name} is a file with extension ${it.extension} with absolute path ${it.absolutePath}")
+            nestedFiles[it.absolutePath] = it.absolutePath.count { ch -> ch == '/' }
+        }
+    }
+    //println(nestedFiles)
+    val theNested = nestedFiles.maxBy { it.value }
+    println("The nested file is ${theNested.key} - ${theNested.key.split('/').last()} with ${theNested.value} nested directories")
+    /*
+        nestedDir.walkTopDown().forEach {
+            if (it.isFile) {
+                val extension = it.extension
+                if (nestedFiles.containsKey(extension)) {
+                    nestedFiles[extension] = nestedFiles[extension]!! + 1
+                } else {
+                    nestedFiles[extension] = 1
+                }
+            }
+        }
+    */
 }
+
+//fun howNested(dir:String, )
